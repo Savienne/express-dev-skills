942d96837cd1d8d8a86aa4f2df5a259dfdfcc425
Savienne <Saviennes.mitchell@yahoo.com>
2022-06-15 11:50:34 -0400

    complete lab

---
+++ express-dev-skillss/data/skills-data.js
@@ -0,0 +1,17 @@
+const skills = [
+
+    {text: 'Fast typing',  done:true},
+    {text: 'Knowing CSS', done:true},
+    {text: 'Knowing Javascript', done: true},
+    
+    
+    
+    
+    
+    
+    
+]
+
+export {
+    skills  
+}
