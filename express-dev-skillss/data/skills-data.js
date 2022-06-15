233397359e65ed7da0804e7ee3ff5247df458966
Savienne <Saviennes.mitchell@yahoo.com>
2022-06-15 14:16:28 -0400

    test push

--- express-dev-skillss/data/skills-data.js
+++ express-dev-skillss/data/skills-data.js
@@ -3,13 +3,7 @@ const skills = [
     {text: 'Fast typing',  done:true},
     {text: 'Knowing CSS', done:true},
     {text: 'Knowing Javascript', done: true},
-    
-    
-    
-    
-    
-    
-    
+    {text: 'Knowing Javascriptt', done: true},
 ]
 
 export {
